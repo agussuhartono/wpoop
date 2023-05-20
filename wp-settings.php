5d0447d36277d361ac34cd7f641938d7570f8572
Agus Suhartono <agus.suhartono@gmail.com>
2023-05-21 04:47:55 +0700

    Remove direct load WP_Application_Passwords (require class-wp-application-passwords.php)
    Remove direct load WP_Application_Passwords
    
    require class-wp-application-passwords.php
    
    from wp-settings.php

--- wp-settings.php
+++ wp-settings.php
@@ -255,7 +255,7 @@ require ABSPATH . WPINC . '/class-wp-widget-factory.php';
 require ABSPATH . WPINC . '/nav-menu-template.php';
 require ABSPATH . WPINC . '/nav-menu.php';
 require ABSPATH . WPINC . '/admin-bar.php';
-require ABSPATH . WPINC . '/class-wp-application-passwords.php';
+
 require ABSPATH . WPINC . '/rest-api.php';
 require ABSPATH . WPINC . '/rest-api/class-wp-rest-server.php';
 require ABSPATH . WPINC . '/rest-api/class-wp-rest-response.php';

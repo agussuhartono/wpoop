3901857a1280e6e0e1485375712b7d4687804417
Agus Suhartono <agus.suhartono@gmail.com>
2023-05-22 14:59:23 +0700

    Remove direct load WP_Block_Pattern_Categories_Registry (require ABSPATH . WPINC . '/class-wp-block-pattern-categories-registry.php';) from wp-settings.php
    Remove direct load WP_Block_Pattern_Categories_Registry
    
    (require ABSPATH . WPINC . '/class-wp-block-pattern-categories-registry.php';)
    
    from wp-settings.php

--- wp-settings.php
+++ wp-settings.php
@@ -316,7 +316,7 @@ require ABSPATH . WPINC . '/sitemaps/providers/class-wp-sitemaps-taxonomies.php'
 require ABSPATH . WPINC . '/sitemaps/providers/class-wp-sitemaps-users.php';
 
 require ABSPATH . WPINC . '/class-wp-block-type.php';
-require ABSPATH . WPINC . '/class-wp-block-pattern-categories-registry.php';
+
 require ABSPATH . WPINC . '/class-wp-block-patterns-registry.php';
 require ABSPATH . WPINC . '/class-wp-block-styles-registry.php';
 require ABSPATH . WPINC . '/class-wp-block-type-registry.php';

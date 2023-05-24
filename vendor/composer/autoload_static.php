7d0005de1f428ef8ba9dbd6a28f14494f978fe2f
Agus Suhartono <agus.suhartono@gmail.com>
2023-05-24 10:38:28 +0700

    Remove direct load WP_Block_Supports (wp-includes/class-wp-block-supports.php)
    Remove direct load WP_Block_Supports (wp-includes/class-wp-block-supports.php)

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -60,10 +60,9 @@ class ComposerStaticInit1a1923b433bd2224dbfd6b2925e4f639
         'WP_Block_Parser_Frame' => __DIR__ . '/../..' . '/wp-includes/class-wp-block-parser.php', // OK
         'WP_Block_Pattern_Categories_Registry' => __DIR__ . '/../..' . '/wp-includes/class-wp-block-pattern-categories-registry.php', // OK
         'WP_Block_Patterns_Registry' => __DIR__ . '/../..' . '/wp-includes/class-wp-block-patterns-registry.php', // OK
-
         'WP_Block_Styles_Registry' => __DIR__ . '/../..' . '/wp-includes/class-wp-block-styles-registry.php', // OK
+        'WP_Block_Supports' => __DIR__ . '/../..' . '/wp-includes/class-wp-block-supports.php', // OK
 
-        'WP_Block_Supports' => __DIR__ . '/../..' . '/wp-includes/class-wp-block-supports.php',
         'WP_Block_Template' => __DIR__ . '/../..' . '/wp-includes/class-wp-block-template.php',
         'WP_Block_Type' => __DIR__ . '/../..' . '/wp-includes/class-wp-block-type.php',
         'WP_Block_Type_Registry' => __DIR__ . '/../..' . '/wp-includes/class-wp-block-type-registry.php',

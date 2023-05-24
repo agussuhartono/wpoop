90594367b667b3f71f8d85b03c17a02c26c433bc
Agus Suhartono <agus.suhartono@gmail.com>
2023-05-24 12:57:24 +0700

    Update autoload_static.php

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -64,8 +64,7 @@ class ComposerStaticInit1a1923b433bd2224dbfd6b2925e4f639
         'WP_Block_Supports' => __DIR__ . '/../..' . '/wp-includes/class-wp-block-supports.php', // OK
         'WP_Block_Template' => __DIR__ . '/../..' . '/wp-includes/class-wp-block-template.php', // OK
         'WP_Block_Type' => __DIR__ . '/../..' . '/wp-includes/class-wp-block-type.php', // OK
-
-        'WP_Block_Type_Registry' => __DIR__ . '/../..' . '/wp-includes/class-wp-block-type-registry.php',
+        'WP_Block_Type_Registry' => __DIR__ . '/../..' . '/wp-includes/class-wp-block-type-registry.php', // OK
 
         'WP_Comment' => __DIR__ . '/../..' . '/wp-includes/class-wp-comment.php',
         'WP_Comment_Query' => __DIR__ . '/../..' . '/wp-includes/class-wp-comment-query.php',

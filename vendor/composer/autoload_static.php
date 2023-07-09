389bc222c81962f6e35b6ad5d8269cd25a9a5d7c
Agus Suhartono <agus.suhartono@gmail.com>
2023-07-09 08:31:21 +0700

    remove direct call class-wp-rewrite.php. user composer autoloader instead.
    remove direct call class-wp-rewrite.php. user composer autoloader instead.

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -122,7 +122,7 @@ class ComposerStaticInit1a1923b433bd2224dbfd6b2925e4f639
         'WP_Recovery_Mode_Key_Service' => __DIR__ . '/../..' . '/wp-includes/class-wp-recovery-mode-key-service.php', // ok
         'WP_Recovery_Mode_Link_Service' => __DIR__ . '/../..' . '/wp-includes/class-wp-recovery-mode-link-service.php', // ok
         
-        'WP_Rewrite' => __DIR__ . '/../..' . '/wp-includes/class-wp-rewrite.php',
+        'WP_Rewrite' => __DIR__ . '/../..' . '/wp-includes/class-wp-rewrite.php', // ok
         'WP_Role' => __DIR__ . '/../..' . '/wp-includes/class-wp-role.php',
         'WP_Roles' => __DIR__ . '/../..' . '/wp-includes/class-wp-roles.php',
 
--- wp-settings.php
+++ wp-settings.php
@@ -194,7 +194,9 @@ require ABSPATH . WPINC . '/comment.php';
 require ABSPATH . WPINC . '/class-walker-comment.php';
 require ABSPATH . WPINC . '/comment-template.php';
 require ABSPATH . WPINC . '/rewrite.php';
-require ABSPATH . WPINC . '/class-wp-rewrite.php';
+
+// require ABSPATH . WPINC . '/class-wp-rewrite.php';
+
 require ABSPATH . WPINC . '/feed.php';
 require ABSPATH . WPINC . '/bookmark.php';
 require ABSPATH . WPINC . '/bookmark-template.php';

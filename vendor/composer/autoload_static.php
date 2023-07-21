6adfe304e5e0ec2d39e11f0f705318e64aee1902
Agus Suhartono <agus.suhartono@gmail.com>
2023-07-21 17:03:29 +0700

    PART 2 : Remove direct call 'Language_Pack_Upgrader' => __DIR__ . '/../..' . '/wp-admin/includes/class-language-pack-upgrader.php' .
    PART 2 : Remove direct call 'Language_Pack_Upgrader' => __DIR__ . '/../..' . '/wp-admin/includes/class-language-pack-upgrader.php' .

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -646,7 +646,7 @@ class ComposerStaticInit1a1923b433bd2224dbfd6b2925e4f639
          */
         'PclZip' => __DIR__ . '/../..' . '/wp-admin/includes/class-pclzip.php',
 
-        'Plugin_Installer_Skin' => __DIR__ . '/../..' . '/wp-admin/includes/class-plugin-installer-skin.php',
+        'Plugin_Installer_Skin' => __DIR__ . '/../..' . '/wp-admin/includes/class-plugin-installer-skin.php', // OK
         'Plugin_Upgrader' => __DIR__ . '/../..' . '/wp-admin/includes/class-plugin-upgrader.php',
         'Plugin_Upgrader_Skin' => __DIR__ . '/../..' . '/wp-admin/includes/class-plugin-upgrader-skin.php',
 
